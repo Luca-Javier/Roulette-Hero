02263e0f35d15189c37fb5650a767fb2b3807d42
Luca-Javier <luca.jav45@gmail.com>
2023-04-10 14:24:00 -0300

    css fix

--- src/components/InteractivePlayerUI.jsx
+++ src/components/InteractivePlayerUI.jsx
@@ -2,11 +2,15 @@ const InteractivePlayerUI = ({}) => {
   return (
     <section className="interactive-player-ui">
       <article className="interactive-sections">
-        <div>1</div>
+        <div className="isActive">1</div>
         <div>2</div>
       </article>
       <article className="interactive-per-section">
         <div>hoal</div>
+        {/* sections/UserStat
+          sections/backpag
+        
+        */}
       </article>
     </section>
   )
