b60ddb92f1b045dce20ec5c52e56da975fa145ae
Luca-Javier <luca.jav45@gmail.com>
2023-05-03 14:42:08 -0300

    added uuid and rollup-alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import alias from "@rollup/plugin-alias"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    alias({
+      entries: [
+        { find: "@assets", replacement: "./src/assets" },
+        { find: "@components", replacement: "./src/components" },
+        { find: "@reducers", replacement: "./src/reducers" },
+        { find: "@helpers", replacement: "./src/helpers" },
+        { find: "@hooks", replacement: "./src/hooks" },
+        { find: "@config", replacement: "./src/config" },
+      ],
+    }),
+  ],
 })
